51c677f4d2db63ec9185a1eda46eb1d04b012bdd
Gubera24 <114172003+Gubera24@users.noreply.github.com>
2023-04-06 19:41:59 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Gubera24/File-Sharing-Bot'>Click here</a>\n○ Channel : <a href='https://t.me/+C3s7faFLSsU3NDQ1'>Tamil Cartoon All</a>\n○ Support Group : @Anime_Chat_Group_Tamil",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Gubera24/File-Share-Bot'>Click here</a>\n○ Channel : <a href='https://t.me/+EYpr8O_Li9BlNTA1'>Horror Movie's Tamil</a>\n○ Support Group : @Tamilan41",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
